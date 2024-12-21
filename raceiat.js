e8b72b43266d25befa3f1da00e74ab711a8ca188
ortukcagrisim2 <ortukcagrisim2@gmail.com>
2024-12-21 23:27:44 +0300

    Update raceiat.js

--- raceiat.js
+++ raceiat.js
@@ -161,7 +161,7 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
             stimulusMedia : [ //Stimuli content as PIP's media objects
-                 {word:'Başkası'},
+                {word:'Başkası'},
                 {word:'Diğerleri'},
                 {word:'Yabancılar'},
                 {word:'Uzaktakiler'},
